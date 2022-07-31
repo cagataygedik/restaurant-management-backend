505dcc7d16e53edc0c6ef56f47ea5a65db359e97
cagataygedik <cagatay.gedik@icloud.com>
2022-07-31 19:08:42 +0300

    second commit/31.07.22

---
+++ controllers/menuController.go
@@ -0,0 +1,53 @@
+package controllers
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/cagataygedik/restaurant-management-backend/database"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
+
+func GetMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing the menu item"})
+		}
+		var allMenus []bson.M
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
+	}
+}
+
+func GetMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func inTimeSpan(start, end, check time.Time) bool {
+	return start.After(time.Now()) && end.After(start)
+}
+
+func UpdateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
